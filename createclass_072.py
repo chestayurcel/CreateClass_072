5920b41394428f4a4d22c2c9249252210bf32572
Yurcell <chestayurcel@gmail.com>
2024-10-30 16:14:11 +0700

    .

--- createclass_072.py
+++ createclass_072.py
@@ -1,16 +1,15 @@
-# Membuat kelas PersegiPanjang
 class PersegiPanjang:
-    def __init__(self, panjang, lebar): # Konstruktor untuk menginisialisasi
+    def __init__(self, panjang, lebar):
         self.panjang = panjang
         self.lebar = lebar
 
-    def keliling(self): # Metode menghitung keliling
+    def keliling(self):
         return 2 * (self.panjang + self.lebar)
     
-    def luas(self): # Metode menghitung luas
+    def luas(self):
         return self.panjang * self.lebar
     
-    def __str__(self): # Metode untuk string
+    def __str__(self):
         return f"Persegi Panjang dengan Panjang {self.panjang} cm, dan Lebar {self.lebar} cm"
     
 pp = PersegiPanjang(8, 5)
